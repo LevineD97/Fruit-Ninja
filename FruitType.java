2520d1dd29431e5256a82a1b4f3c592313801700
LevineD97 <dylanlev@seas.upenn.edu>
2016-12-31 02:34:47 -0500

    Delete FruitType.java

--- FruitType.java
+++
@@ -1,6 +0,0 @@
-public enum FruitType {Apple, Mango, Pineapple, Strawberry, Watermelon;
-   
-    public static FruitType getRandom() {
-        return values()[(int) (Math.random() * values().length)];
-    }
-}
